326a45ede4fcc39b301c936f6275ebc3cf1f73a6
naveenasok <naveenasokan46@gmail.com>
2021-07-23 15:03:27 +0530

    Task is compeleted

---
+++ src/test/java/org/Test/SampleClass.java
@@ -0,0 +1,13 @@
+package org.Test;
+
+public class SampleClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("java");
+		System.out.println("python");
+		System.out.println("c++");
+		
+	}
+	
+}
